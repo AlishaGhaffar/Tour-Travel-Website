d24f9b09efe90cdee2f271e19c966190b1ca344d
Alisha Ghaffar <alishaghaffar73@gmail.com>
2025-07-07 12:44:39 +0500

    changes in singup form

--- src/routes/SignUp.js
+++ src/routes/SignUp.js
@@ -3,14 +3,7 @@ import SignUpForm from "../components/SignUpForm";
 
 function SignUp() {
     return (
-        <>
-        
-            <SignUpForm
-                cName="hero-mid"
-                title="About"
-                btnClass="hide"
-            />
-        </>
+        <SignUpForm />
     )
 }
 
